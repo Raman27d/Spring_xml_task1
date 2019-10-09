d9be07b16abcefb728bc052b92e6d8e2917a0b26
Raman27d <ramandhin997@gmail.com>
2019-10-09 15:47:00 +0530

    task3

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -1,5 +1,6 @@
 package com.stackroute;
 
+import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
